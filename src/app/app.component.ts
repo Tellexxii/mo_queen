72e9bf37e857006c98e3c6a4eb911e0f5b50d768
Ирина Пупкова <pupkova.ia@yandex.ru>
2024-06-28 11:42:09 +0500

    [upd] transfer base template from base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,35 +2,61 @@ import { Component } from '@angular/core';
 import { ElectronService } from './core/services';
 import { TranslateService } from '@ngx-translate/core';
 import { APP_CONFIG } from '../environments/environment';
-import {LocalServerConfig} from "../../app/src/server";
+import { LocalServerConfig } from "../../app/src/server";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(
-    private electronService: ElectronService,
-    private translate: TranslateService
-  ) {
-    this.translate.setDefaultLang('en');
-    console.log('APP_CONFIG', APP_CONFIG);
-    const config: LocalServerConfig = {port: 3001};
+    constructor(
+        private electronService: ElectronService,
+        private translate: TranslateService
+    ) {
+        this.translate.setDefaultLang('en');
+        console.log('APP_CONFIG', APP_CONFIG);
+        const config: LocalServerConfig = {port: 3001};
 
-    if (electronService.isElectron) {
-      console.log(process.env);
-      console.log('Run in electron');
-      console.log('Electron ipcRenderer', this.electronService.ipcRenderer);
-      console.log('NodeJS childProcess', this.electronService.childProcess);
-      this.electronService.ipcRenderer.invoke('create-server', config)
-        .then(() => console.log('started'))
-        .catch((err) => console.log(err))
-      this.electronService.ipcRenderer.invoke('start-server')
-        .then(() => console.log('started'))
-        .catch((err) => console.log(err))
-    } else {
-      console.log('Run in browser');
+        if (electronService.isElectron) {
+            console.log(process.env);
+            console.log('Run in electron');
+            console.log('Electron ipcRenderer', this.electronService.ipcRenderer);
+            console.log('NodeJS childProcess', this.electronService.childProcess);
+            this.electronService.ipcRenderer.invoke('create-server', config)
+                .then(() => console.log('started'))
+                .catch((err) => console.log(err))
+            this.electronService.ipcRenderer.invoke('start-server')
+                .then(() => console.log('started'))
+                .catch((err) => console.log(err))
+        } else {
+            console.log('Run in browser');
+        }
     }
-  }
+
+    start() {
+        console.log('Start');
+    }
+
+    stop() {
+        console.log('Stop');
+    }
+
+    onPortChange(evt: Event) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        console.log('port', evt.target['value'])
+    }
+
+    onControllerNameChange(evt: Event) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        console.log('Controller', evt.target['value'])
+    }
+
+    onJSONChange(evt: Event) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        console.log('JSON', evt.target['value'])
+    }
 }
